d3bc7292e9e7a34abb209fd9d0bb157002daa857
Mahamrithunjaya <sahasubhajitias59@gmail.com>
2022-12-13 21:22:02 +0530

    Updated

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
           <p class="mandate-fields">* Fields are mandatory</p>
             <div class="input-group">
               <label><strong>Salutation: </strong></label>
-                <select name="salutation" id="salut" value="<?php if (isset($salutation) && $success == '') {echo $salutation;}?>">
+                <select name="salutation" id="salut">
                   <option value="" selected="selected" >--Select--</option>
                   <option value="Dr.">Dr.</option>
                   <option value="Mr.">Mr.</option>
@@ -41,78 +41,78 @@
 
           <div class="input-group">
             <label><strong>First Name: <span class="mandate-fields">*</span></strong></label>
-            <input type="text" placeholder="Enter Your First Name" name="fname" value="<?php if (isset($firstname) && $success == '') {echo $firstname;}?>" onkeyup="validateFName()">
+            <input type="text" placeholder="Enter Your First Name" name="fname" value="<?= $firstName ?>"onkeyup="validateFName()">
             <span> <?= $firstNameErr;?></span>
           </div>
     
           <div class="input-group">
             <label><strong>Last Name: *</strong></label>
-            <input type="text" placeholder="Enter Your Last Name" name="lname" value="<?php if (isset($lastName) && $success == '') {echo $lastName;} ?>"onkeyup="validateLName()">
+            <input type="text" placeholder="Enter Your Last Name" name="lname" value="<?= $lastName ?>"onkeyup="validateLName()">
             <span><?= $lastNameErr;?></span>
           </div>
 
           <div class="input-group">
             <label><strong>Designation: *</strong></label>
-            <input type="text" placeholder="Enter Your Last Name" name="desgnation" value="<?php if (isset($designation) && $success == '') {echo $designation;} ?>" onkeyup="validateLName()">
+            <input type="text" placeholder="Enter Your Last Name" name="desgnation" value="<?= $designation ?>" onkeyup="validateLName()">
             <span><?= $designationErr;?></span>
           </div>
           <div class="input-group">
             <label><strong>Affilation:</strong></label>
-            <input type="text" placeholder="Enter Your Last Name" name="affliation" value="<?php if (isset($affiliation) && $success == '') {echo $affiliation;} ?>"onkeyup="validateLName()">
+            <input type="text" placeholder="Enter Your Last Name" name="affliation" value="<?= $affiliation ?>"onkeyup="validateLName()">
             <span>*<?= $affiliationErr;?></span>
           </div>
     
           <div class="input-group">
             <label><strong>Gender:</strong></label>
-            <input type="radio" style="flex-basis: 0%; margin: 0 10px;" name="gender" value="Male" value="<?php if (isset($gender) && $success == '') {echo $gender;} ?>">
+            <input type="radio" style="flex-basis: 0%; margin: 0 10px;" name="gender" value="Male" value="<?= $gender ?>">
             <label>Male</label> 
-            <input type="radio" style="flex-basis: 0%; margin: 0 10px;"name="gender" value="Female" value="<?php if (isset($gender) && $success == '') {echo $gender;} ?>">
+            <input type="radio" style="flex-basis: 0%; margin: 0 10px;"name="gender" value="Female" value="<?= $gender ?>">
             <label>Female</label> 
             <span id="gender-error"><?= $genderErr;?></span>
           </div>
 
           <div class="input-group">
             <label><strong>Candidature:</strong></label>
-            <input type="radio" style="flex-basis: 0%; margin: 0 10px;" name="candidature" value="Presenter" value="<?php if (isset($candidature) && $success == '') {echo $candidature;} ?>">
+            <input type="radio" style="flex-basis: 0%; margin: 0 10px;" name="candidature" value="Presenter" value="<?= $candidature ?>">
             <label>Presenter</label> 
-            <input type="radio" style="flex-basis: 0%; margin: 0 10px;" name="candidature" value="Participant" value="<?php if (isset($candidature) && $success == '') {echo $candidature;} ?>">
+            <input type="radio" style="flex-basis: 0%; margin: 0 10px;" name="candidature" value="Participant" value="<?= $candidature ?>">
             <label>Participant</label> 
             <span id="gender-error"></span>
           </div> 
 
           <div class="input-group">
             <label><strong>Registration Fees:</strong></label>
-            <input type="text" placeholder="Enter Your Last Name" name="regfee" value="<?php if (isset($regFees) && $success == '') {echo $regFees;} ?>">
+            <input type="text" placeholder="Enter Your Last Name" name="regfee" value="<?= $regFees ?>">
             <span id="lname-error"><?= $regFeesErr;?></span>
           </div>
 
           <div class="input-group">
             <label><strong>Payment Slip:</strong></label>
-            <input type="file" accept="image/*,.pdf" name="payslip" value="<?php if (isset($paySlip) && $success == '') {echo $paySlip;} ?>">
+            <input type="file" accept="image/*,.pdf" name="payslip" value="<?= $paySlip ?>">
             <span id="lname-error"></span>
           </div>
 
           <div class="input-group">
             <label><strong>Contact Details:</strong></label>
-            <input type="text" placeholder="Enter Your Address" name="contact-details" value="<?php if (isset($contact) && $success == '') {echo $contact;} ?>">
+            <input type="text" placeholder="Enter Your Address" name="contact-details" value="<?= $contact ?>">
             <span id="address-error"></span>
           </div>
 
           <div class="input-group">
             <label><strong>Email Id:</strong></label>
-            <input type="email" placeholder="Enter Your Email Id" name="email-id" value="<?php if (isset($email) && $success == '') {echo $email;} ?>">
+            <input type="email" placeholder="Enter Your Email Id" name="email-id" value="<?= $email ?>">
             <span id="email-error"><?= $emailErr;?></span>
           </div>
 
           <div class="input-group">
             <label><strong>Mobile No.:</strong></label>
-            <input type="tel" placeholder="Enter your mobile number" name="phone-no" value="<?php if (isset($phoneNo) && $success == '') {echo $phoneNo;} ?>">
+            <input type="tel" placeholder="Enter your mobile number" name="phone-no" value="<?= $phoneNo ?>">
             <span id="mobile-error"><?= $phoneNoErr;?></span>
           </div>
 
           <div class="input-group">
             <label><strong>Food Preference:</strong></label>
-            <select name="food-pref" value="<?php if (isset($food) && $success == '') {echo $food;} ?>">
+            <select name="food-pref" value="<?= $food ?>">
               <option value="q" selected="selected" >--Select--</option>
               <option value="Veg">VEG</option>
               <option value="Non Veg">NON VEG</option>
@@ -121,7 +121,7 @@
 
           <div class="input-group">
             <label><strong>Accomodation:</strong></label>
-            <select name="accomodation-pref" value="<?php if (isset($accomodation) && $success == '') {echo $accomodation;} ?>">
+            <select name="accomodation-pref" value="<?= $accomodation ?>">
               <option value="q" selected="selected">--Select--</option>
               <option value="Required">REQUIRED</option>
               <option value="Not required">NOT REQUIRED</option>
